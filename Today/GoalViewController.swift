1cd884d4966e44f057032ba7493edbe84d279ffa
Roger Chen <rogerthechen@gmail.com>
2015-03-01 14:23:38 -0800

    First commit, starting the GoalViewController

---
+++ Today/GoalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GoalViewController.swift
+//  Today
+//
+//  Created by Roger Chen on 3/1/15.
+//  Copyright (c) 2015 Roger Chen. All rights reserved.
+//
+
+import UIKit
+
+class GoalViewController: UIViewController
+{
+    
+    override func viewDidLoad() {
+
+    }
+
+}
